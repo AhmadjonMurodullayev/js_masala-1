a0255a43402fc60f7a0551657781061ace3a6597
AhmadjonMurodullayev <murodullayevahmadjon26@gmail.com>
2024-06-29 21:24:44 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,157 @@
+// function caunt (number){
+//     let result = ""
+//    if !isNaN (number) {
+//     if(number % 2 == 0){
+//         result ='${number} juft son'
+//     } else(number % 2 ==0){
+//         result ='${number} juft son'
+//     }
+//    }
+//    return result
+// }
+
+
+
+
+
+
+// function  totalCont (number){
+//     let result =""
+//     if (! isNaN (number)){
+//         if (number % 2 == 0){
+//             result = '$ {number} juft son'
+//         }else{
+//           result = '$ {number} toq son'
+//         }
+//     }else{
+//         result ='string kiriting'
+//     }
+//     return number ? result : "hech narsa kiritilmagan"
+// }
+//  totalCont()
+
+
+
+// let number = prompt ("son kiriting")
+// let rabdom = Math.floor(Math.random () * 10)
+// const result = () => {
+//     if (number > random){
+//         console.log '$ {number} son katta'
+//     }else( number < rabdom){
+//           console.log'$ {random} son katta'
+//     }
+// }
+
+
+///////
+
+
+
+// function maxValue (a , b, c) {
+//     if(a > b && a > c ){
+//         return a;
+//     } else if (b > a && b > c){
+//         return b;
+//     } else {
+//         return c ;
+//     }
+// }
+// console.log(maxValue(a, b, c))
+
+// const maxValue1 (a , b, c) => {
+//     if(a > b && a > c ){
+//         return a;
+//     } else if (b > a && b > c){
+//         return b;
+//     } else {
+//         return c ;
+//     }
+// }
+// console.log(maxValue1(20, 30, 11))
+
+
+
+// //////
+
+
+
+// function degree (b,d){
+//     return b  ** d ;
+// }
+// console.log(degree(2,4));
+
+
+
+
+
+// //////
+
+
+
+
+
+
+// function degree (a,b){
+//     if ( a > b){
+//         return a % b ;
+//     }else {
+//         return b % a ;
+//     }
+    
+// }
+// console.log(degree(15,4));
+
+
+
+// ////
+
+// function checkFizzBazz(number){
+//         if ( number % 5 == 0 && number % 3 ==0){
+//             return ("chekFizzBazz")
+//         }else if(number % 5 ==0){
+//             return("Fizz")
+
+//         }else if (number % 3 ==0){
+//             return ("Bazz")
+//         }
+           
+//     }
+
+
+
+
+
+// ////
+    // const getRoom = function (mA){
+    //     if(mA < 1000){
+    //         return("lux")
+    //     }else(mA < 500){
+    //     return("yarim lux")
+    //     }else (mA < 200){
+    //         return("odiiy")
+    //     }else{
+    //         return(puingiz yetmaydi)
+    //     }
+    // }
+    
+
+    // /////
+
+    const  getGranf =function(sorch){
+
+        if(sorch < 5){
+                    return("Alo")
+                }else(sorch < 4){
+                return("yaxshi")
+                }else (sorch < 3){
+                    return("qoniqarli")
+                }else{
+                    return(bunday baho yuq)
+                }
+
+    }
+
+
+
+
+
